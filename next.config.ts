a872e6b27dbd304f6c2fc99a1576df19438cdacb
Rupam Sarkar <rupam.sarkar@outlook.in>
2025-06-02 22:59:50 +0530

    css issue

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,9 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  output: 'export'
+  output: 'export',
+  basePath: '/chessable',
+  assetPrefix: '/chessable/',
 };
 
 export default nextConfig;
